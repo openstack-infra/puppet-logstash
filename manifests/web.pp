a240bd6421ba6d5cccbd594d5cc6b253f9f60e23
K Jonathan Harker <k.jonathan.harker@hp.com>
2015-08-24 16:08:22 -0700

    Upgrade from kibana2 to kibana3
    Change-Id: I176148c3bdaf603a1703d215ac2086101ecabe21
    Depends-On: I13f9dff0bbd6498a36dc75b026c9042a9bb05e8f

--- manifests/web.pp
+++ manifests/web.pp
@@ -82,21 +82,15 @@ class logstash::web (
 
     'kibana': {
       class { '::kibana':
-        discover_nodes => $discover_nodes,
+        version                 => 'js',
+        discover_nodes          => $discover_nodes,
+        js_elasticsearch_prefix => '/elasticsearch/',
+        js_elasticsearch_url    => "http://${discover_nodes[0]}:9200",
       }
-      $vhost = 'logstash/kibana.vhost.erb'
     }
 
     default: {
       fail("Unknown frontend to logstash: ${frontend}.")
     }
-  }
-
-  ::httpd::vhost { $vhost_name:
-    port       => 80,
-    docroot    => 'MEANINGLESS ARGUMENT',
-    priority   => '50',
-    template   => $vhost,
-    vhost_name => $vhost_name,
   }
 }
