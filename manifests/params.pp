0990d9be19308c8b5f09958aa7c1f4c64574e9ca
Colleen Murphy <colleen@gazlene.net>
2017-06-11 17:43:53 +0200

    Fix beaker on xenial
    Add a xenial nodeset and update the spec helper to install puppet 3 from
    the Ubuntu repos instead of from puppetlabs.
    
    Change-Id: Ib0e2a88e4e43300057183f873d56faf1e1aab5e8

---
+++ manifests/params.pp
@@ -0,0 +1,12 @@
+# logstash::params
+class logstash::params(
+) {
+  case $::lsbrelease {
+    'xenial' {
+      $jre_package = 'openjdk-8-jre-headless'
+    }
+    default {
+      $jre_package = 'openjdk-7-jre-headless'
+    }
+  }
+}
