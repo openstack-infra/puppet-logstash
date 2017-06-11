38c66fc220ccbed4c4fe7e29ec7e19cb286a5583
Colleen Murphy <colleen@gazlene.net>
2017-06-11 19:21:41 +0200

    Remove unnecessary beaker tests
    We do not need to check every package and file with beaker. It is safe
    to assume that if puppet ran successfully then it also was able to find
    and install every package and was able to apply every config file.
    
    We still keep the service checks because it is possible for puppet to
    declare a service to be running successfully only for it to die later.
    
    Change-Id: I313fe7907b165e4c064cc37cf0c75051aabf103e

--- spec/acceptance/basic_spec.rb
+++ spec/acceptance/basic_spec.rb
@@ -19,48 +19,6 @@ describe 'puppet-logstash module', :if => ['debian', 'ubuntu'].include?(os[:fami
     apply_manifest(default_puppet_module, catch_changes: true)
   end
 
-  describe 'require files' do
-    describe 'module logstash::elasticsearch' do
-      describe file('/etc/elasticsearch/templates/logstash_settings.json') do
-        it { should be_file }
-        it { should be_owned_by 'root' }
-        it { should be_grouped_into 'root' }
-        its(:content) { should include '"index.cache.field.type" : "soft"' }
-      end
-    end
-
-    describe 'module logstash::indexer' do
-      describe file('/etc/logstash/conf.d/00-input.conf') do
-        it { should be_file }
-        it { should be_owned_by 'logstash' }
-        it { should be_grouped_into 'logstash' }
-        its(:content) { should include 'type => "redis-input"' }
-        its(:content) { should include 'host => "127.0.0.1"' }
-      end
-
-      describe file('/etc/logstash/conf.d/99-output.conf') do
-        it { should be_file }
-        it { should be_owned_by 'logstash' }
-        it { should be_grouped_into 'logstash' }
-        its(:content) { should include 'host => "127.0.0.1"' }
-      end
-
-      describe file('/etc/default/logstash') do
-        its(:content) { should include 'LS_OPTS="-w 1"' }
-      end
-    end
-  end
-
-  describe 'required package' do
-    describe package('openjdk-8-jre-headless') do
-      it { should be_installed }
-    end
-
-    describe package('logstash') do
-      it { should be_installed }
-    end
-  end
-
   describe 'required services' do
     describe service('logstash') do
       it { should be_enabled }
